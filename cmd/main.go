822a09123283ab8bcb7b246cfb0054fb5f74c912
fabian <fbngrm@gmail.com>
2022-06-28 12:31:49 +0200

    clean up setup

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"text/template"
 
 	"github.com/fgrimme/zh/internal/cedict"
 	"github.com/fgrimme/zh/internal/cjkvi"
@@ -18,11 +17,10 @@ const cedictSrc = "./lib/cedict/cedict_1_0_ts_utf-8_mdbg.txt"
 
 var query string
 var templatePath string
+var format string
 var interactive bool
 var results int
 var depth int
-var jsonOut bool
-var yamlOut bool
 var unihanSearch bool
 
 var fields string
@@ -31,21 +29,12 @@ func main() {
 	flag.StringVar(&query, "q", "", "query")
 	flag.StringVar(&fields, "f", "", "filter fields")
 	flag.StringVar(&templatePath, "t", "", "go template")
+	flag.StringVar(&format, "fmt", "text", "format output [json|yaml|text]")
 	flag.BoolVar(&interactive, "i", false, "interactive search")
-	flag.BoolVar(&jsonOut, "j", false, "output in json format")
-	flag.BoolVar(&yamlOut, "y", false, "output in yaml format")
 	// flag.BoolVar(&unihanSearch, "u", false, "force search in unihan db (single hanzi only)")
 	flag.IntVar(&results, "r", 3, "number of results")
 	flag.IntVar(&depth, "d", 1, "decomposition depth")
 	flag.Parse()
-
-	// filter
-	if fields != "" {
-		if !jsonOut && !yamlOut {
-			fmt.Println("can use field filter only with json or yaml format")
-			os.Exit(1)
-		}
-	}
 
 	dict, err := cedict.NewDict(cedictSrc)
 	if err != nil {
@@ -70,26 +59,17 @@ func main() {
 	}
 
 	// out
-	if templatePath != "" {
-		tmpl, err := template.ParseFiles(templatePath)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		tmpl.Execute(os.Stdout, []*hanzi.Hanzi{h})
-		os.Exit(0)
-	}
-
-	format := hanzi.Format_plain
-	if jsonOut {
-		format = hanzi.Format_JSON
-	} else if yamlOut {
-		format = hanzi.Format_YAML
-	}
 	formatter := hanzi.NewFormatter(
-		hanzi.Format(format),
+		format,
 		fields,
 	)
-	formatted, err := formatter.Format(h)
+
+	var formatted string
+	if templatePath != "" {
+		formatted, err = formatter.FormatTemplate(h, fields, templatePath)
+	} else {
+		formatted, err = formatter.Format(h, fields)
+	}
 	if err != nil {
 		fmt.Printf("could not format hanzi: %v\n", err)
 		os.Exit(1)
--- internal/hanzi/format.go
+++ internal/hanzi/format.go
@@ -1,9 +1,11 @@
 package hanzi
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"strings"
+	"text/template"
 
 	"gopkg.in/yaml.v3"
 )
@@ -13,42 +15,51 @@ type Format int
 const (
 	Format_JSON = iota
 	Format_YAML
-	Format_plain
+	Format_text
 )
 
 type Formatter struct {
-	format       Format
-	filterFields []string
+	format Format
 }
 
-func NewFormatter(f Format, fields string) *Formatter {
+func NewFormatter(fmt, fields string) *Formatter {
 	return &Formatter{
-		format:       f,
-		filterFields: prepareFilter(fields),
+		format: format(fmt),
 	}
 }
 
-func (f *Formatter) Format(hanzi *Hanzi) (string, error) {
-	var data interface{}
-	if len(f.filterFields) != 0 {
-		var err error
-		data, err = hanzi.GetFields(f.filterFields)
-		if err != nil {
-			return "", fmt.Errorf("could not filter fields: %w", err)
-		}
-	} else {
-		data = hanzi
+func (f *Formatter) FormatTemplate(h *Hanzi, fields, tmplPath string) (string, error) {
+	i, err := f.filter(h, fields)
+	if err != nil {
+		return "", fmt.Errorf("could not filter fields: %w", err)
+	}
+	tmpl, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		return "", err
+	}
+	buf := new(bytes.Buffer)
+	err = tmpl.Execute(buf, []*Hanzi{i.(*Hanzi)})
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+func (f *Formatter) Format(h *Hanzi, fields string) (string, error) {
+	i, err := f.filter(h, fields)
+	if err != nil {
+		return "", fmt.Errorf("could not filter fields: %w", err)
 	}
 	if f.format == Format_JSON {
-		return f.formatJSON(data)
+		return formatJSON(i)
 	}
 	if f.format == Format_YAML {
-		return f.formatYAML(data)
+		return formatYAML(i)
 	}
-	return f.formatPlain(data)
+	return formatText(i)
 }
 
-func (f *Formatter) formatJSON(data interface{}) (string, error) {
+func formatJSON(data interface{}) (string, error) {
 	b, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
 		return "", err
@@ -56,7 +67,7 @@ func (f *Formatter) formatJSON(data interface{}) (string, error) {
 	return string(b), nil
 }
 
-func (f *Formatter) formatYAML(data interface{}) (string, error) {
+func formatYAML(data interface{}) (string, error) {
 	b, err := yaml.Marshal(data)
 	if err != nil {
 		return "", err
@@ -64,7 +75,7 @@ func (f *Formatter) formatYAML(data interface{}) (string, error) {
 	return string(b), nil
 }
 
-func (f *Formatter) formatPlain(data interface{}) (string, error) {
+func formatText(data interface{}) (string, error) {
 	var result string
 	d, ok := data.(*Hanzi)
 	if !ok {
@@ -82,7 +93,16 @@ func (f *Formatter) formatPlain(data interface{}) (string, error) {
 	return result, nil
 }
 
-func prepareFilter(fields string) []string {
+func format(fmt string) Format {
+	if fmt == "json" {
+		return Format_JSON
+	} else if fmt == "yaml" {
+		return Format_YAML
+	}
+	return Format_text
+}
+
+func (f *Formatter) filter(h *Hanzi, fields string) (interface{}, error) {
 	var filterFields []string
 	if len(fields) > 0 {
 		filterFields = strings.Split(fields, ",")
@@ -90,5 +110,9 @@ func prepareFilter(fields string) []string {
 	for i, field := range filterFields {
 		filterFields[i] = strings.TrimSpace(field)
 	}
-	return filterFields
+
+	if len(fields) != 0 {
+		return h.GetFields(filterFields)
+	}
+	return h, nil
 }
